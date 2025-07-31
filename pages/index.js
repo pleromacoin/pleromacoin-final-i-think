484f4b65ccab0f958a6ba4262786c3787fd63958
pleromacoin <founder@the-sophia-project.com>
2025-07-30 23:11:17 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: '100vh',
+      backgroundColor: '#0a192f',
+      color: '#d4af37',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '2rem',
+      fontFamily: 'Georgia, serif'
+    }}>
+      Pleroma Site Coming Soon
+    </main>
+  );
+}
